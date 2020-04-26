e6c056bb2cc17d6fad643c3c12fc5af84992e9be
fabio <fabiosjordao@gmail.com>
2020-04-26 20:07:20 -0300

    Start

---
+++ src/pages/Main/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  state = {
+    newRepository: '',
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ newRepository: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(this.state.newRepository);
+  };
+
+  render() {
+    const { newRepository } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepository}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
