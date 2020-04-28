9ff088158bca83d00b7249b897d0c10aae646df8
fabio <fabiosjordao@gmail.com>
2020-04-27 22:08:10 -0300

    Proptypes

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>;
+class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }),
+  };
+  state = {
+    repositoryName: '',
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+  async componentDidMount() {
+    const { match } = this.props;
+    const repositoryName = decodeURIComponent(match.params.repository);
+    this.setState({ repositoryName });
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repositoryName}`),
+      api.get(`/repos/${repositoryName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repositoryName } = this.state;
+    return <h1>Repository: {repositoryName} </h1>;
+  }
 }
+
+export default Repository;
