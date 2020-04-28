12b530988ab484f3bb8ced464808bbdcef86ac26
fabio <fabiosjordao@gmail.com>
2020-04-27 22:41:05 -0300

    Separando o container

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,25 +1,5 @@
 import styled, { keyframes, css } from 'styled-components';
 
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  padding: 30px;
-  margin: 80px auto;
-
-  h1 {
-    font-size: 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-
-  svg {
-    margin-right: 10px;
-  }
-`;
-
 export const Form = styled.form`
   margin-top: 30px;
   display: flex;
@@ -86,10 +66,6 @@ export const List = styled.ul`
 
     & + li {
       border-top: 1px solid #eee;
-    }
-    a {
-      color: #3b38d1;
-      text-decoration: none;
     }
   }
 `;
